2ba1f096955a65fe6f04353cdedbff305e0ac2dc
luoxiaoyao <m18356006370@163.com>
2021-08-22 11:17:40 +0800

    nio01

---
+++ week02/06/week02/src/main/java/com/works/week02/nio01/netty/HttpInitializer.java
@@ -0,0 +1,20 @@
+package com.works.week02.nio01.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+
+
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline p = socketChannel.pipeline();
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpServerExpectContinueHandler());
+        p.addLast(new HttpObjectAggregator(1024 * 1024));
+        p.addLast(new HttpHandler());
+    }
+}
